5742f41bb8f4956e6da9007a7b8c419553e6ca86
HuyHieu-19 <104619096+HuyHieu-19@users.noreply.github.com>
2023-08-15 16:48:11 +0700

    update source code for scan

--- data/main.js
+++ data/main.js
@@ -1,5 +1,7 @@
 const navItems = document.querySelectorAll("nav li");
 
+let globalRFData = null;
+let globalScanData = null;
 // one page renderring in navbar
 navItems.forEach((item) => {
    item.addEventListener('click', function () {
@@ -15,6 +17,156 @@ navItems.forEach((item) => {
       this.querySelector('a').classList.add('active');
    })
 })
+
+function sendDataToServer(url, data) {
+   fetch(url, {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+   })
+      .then(response => response.json())
+      .then(data => {
+         console.log("Response from server:", data);
+      })
+      .catch(error => {
+         console.error("Error:", error);
+      });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+   const form = document.getElementById("modulation-form");
+   form.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const modulationOptions = document.querySelectorAll('input[name="modulation"]:checked');
+      const startFreq = document.getElementById("start-freq").value;
+      const freqStep = document.getElementById("freq-step").value;
+
+      const jsonData = {
+         modulation: modulationOptions.length > 0 ? modulationOptions[0].value : null,
+         startFrequency: startFreq,
+         step: freqStep
+      };
+
+      console.log(jsonData);
+      globalScanData = jsonData;
+      sendDataToServer('/post/scan/submit', jsonData)
+   });
+});
+document.addEventListener("DOMContentLoaded", function () {
+   const form = document.getElementById("tx-rx-config-form");
+   form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent form submission
+
+      const modulation = document.getElementById("modulation").value;
+      const basicFrequency = parseFloat(document.getElementById("basicFrequency").value);
+      const frequencyDeviation = parseFloat(document.getElementById("frequencyDeviation").value);
+      const channelNumber = parseInt(document.getElementById("channelNumber").value);
+      const channelSpacing = parseFloat(document.getElementById("channelSpacing").value);
+      const receiveBW = parseFloat(document.getElementById("receiveBW").value);
+      const dataRate = parseFloat(document.getElementById("dataRate").value);
+      const power = parseInt(document.getElementById("power").value);
+      const syncMode = parseInt(document.getElementById("syncMode").value);
+      const syncWordHigh = document.getElementById("syncWordHigh").value;
+      const syncWordLow = document.getElementById("syncWordLow").value;
+      const adrCheck = parseInt(document.getElementById("adrCheck").value);
+      const address = document.getElementById("address").value;
+      const whitening = parseInt(document.getElementById("whitening").value);
+      const pktFormat = parseInt(document.getElementById("pktFormat").value);
+      const lengthConfig = parseInt(document.getElementById("lengthConfig").value);
+      const packetLength = parseInt(document.getElementById("packetLength").value);
+      const crc = parseInt(document.getElementById("crc").value);
+      const crcaf = parseInt(document.getElementById("crcaf").value);
+      const dcfilteroff = parseInt(document.getElementById("dcfilteroff").value);
+      const manchester = parseInt(document.getElementById("manchester").value);
+      const fec = parseInt(document.getElementById("fec").value);
+      const pre = parseInt(document.getElementById("pre").value);
+      const pqt = parseInt(document.getElementById("pqt").value);
+      const appendstatus = parseInt(document.getElementById("appendstatus").value);
+
+      const jsonData = {
+         modulation,
+         basicFrequency,
+         frequencyDeviation,
+         channelNumber,
+         channelSpacing,
+         receiveBW,
+         dataRate,
+         power,
+         syncMode,
+         syncWordHigh,
+         syncWordLow,
+         adrCheck,
+         address,
+         whitening,
+         pktFormat,
+         lengthConfig,
+         packetLength,
+         crc,
+         crcaf,
+         dcfilteroff,
+         manchester,
+         fec,
+         pre,
+         pqt,
+         appendstatus
+      };
+      globalRFData = jsonData;
+      //   console.log(jsonData); // You can replace this with your JSON handling logic
+      sendDataToServer('/post/txrx/submit', jsonData)
+   });
+});
+
+const form = document.getElementById("modulation-form");
+const modulationOptions = form.elements["modulation"];
+const startFreqInput = document.getElementById("start-freq");
+const freqStepInput = document.getElementById("freq-step");
+
+function updateFormWithData(data) {
+   modulationOptions.value = data.modulation;
+   startFreqInput.value = data.startFrequency;
+   freqStepInput.value = data.step;
+}
+let on = 1;
+function fetchDataAndUpdateForm() {
+   fetch(`/get/scan?on=${!on}`)
+      .then(response => response.json())
+      .then(data => {
+         on = !on;
+         updateFormWithData(data);
+      })
+      .catch(error => {
+         console.error("Error:", error);
+      });
+}
+
+// Fetch and update form every second
+
+let intervalId = null;
+const scanRequest = document.getElementById("scanButton");
+scanRequest.addEventListener("click", function (event) {
+   event.preventDefault();
+   intervalsetInterval(fetchDataAndUpdateForm, 1000);
+});
+
+window.addEventListener('beforeunload', () => {
+   clearInterval(intervalId);
+});
+
+const sendRFRequest = document.getElementById("sendRFButton");
+sendRFRequest.addEventListener("click", function (event) {
+   event.preventDefault();
+   sendDataToServer('/post/send/frequency', globalRFData);
+});
+
+
+const receiveRFRequest = document.getElementById("receiveRFButton");
+receiveRFRequest.addEventListener("click", function (event) {
+   event.preventDefault();
+   sendDataToServer('/post/receive/frequency', globalRFData);
+});
 
 function onViewRF() {
    fetch('/on')
